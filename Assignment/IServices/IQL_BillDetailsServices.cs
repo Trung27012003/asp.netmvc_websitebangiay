82e8f1cd535da4151f4cbf871ff04f60ad1bb88c
Trung27012003 <Trungtvph28671@fpt.edu.vn>
2023-04-25 02:34:23 +0700

    Add project files.

---
+++ Assignment/IServices/IQL_BillDetailsServices.cs
@@ -0,0 +1,12 @@
+﻿using Assignment.ViewModels;
+
+namespace Assignment.IServices
+{
+    public interface IQL_BillDetailsServices
+    {
+        public bool CreateBillDetail(QL_BillDetailsViews p);
+        public bool UpdateBillDetail(QL_BillDetailsViews p);
+        public bool DeleteBillDetail(Guid id);
+        public List<QL_BillDetailsViews> GetAllBillDetail();
+    }
+}
---
+++ Assignment/IServices/IQL_BillServices.cs
@@ -0,0 +1,13 @@
+﻿using Assignment.Models;
+using Assignment.ViewModels;
+
+namespace Assignment.IServices
+{
+    public interface IQL_BillServices
+    {
+        public bool CreateBill(QL_BillViews p);
+        public bool UpdateBill(QL_BillViews p);
+        public bool DeleteBill(Guid id);
+        public List<QL_BillViews> GetAllBill();
+    }
+}
